dc3daa81e4f64af10dd645bbee53f79c5a25c6ab
Adrian Glapiński <adriangl1997@gmail.com>
2020-09-29 23:10:29 +0200

    Configure discovery clients

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -4,4 +4,5 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 }
