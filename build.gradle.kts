8162e4115804519c84dc6ed29928fe59d2ebcf34
Adrian Glapiński <adriangl1997@gmail.com>
2020-09-29 23:21:07 +0200

    Add spring-cloud-config

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ subprojects {
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.springframework.cloud:spring-cloud-starter-config")
         testImplementation("org.springframework.boot:spring-boot-starter-test") {
             exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         }
