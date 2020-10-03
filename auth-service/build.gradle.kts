1989bd0fbdf562d956aa53ff94a99fcd4e6f0876
Adrian Glapiński <adriangl1997@gmail.com>
2020-10-03 11:58:07 +0200

    Reorder

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -3,6 +3,6 @@ version = "0.0.1-SNAPSHOT"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	runtimeOnly("org.postgresql:postgresql")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	runtimeOnly("org.postgresql:postgresql")
 }
