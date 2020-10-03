d4eb3f26f90ab19d4f4ed8be685f3af748d655c3
Adrian Glapiński <adriangl1997@gmail.com>
2020-10-03 15:35:59 +0200

    Configure properties

--- mail-service/src/main/kotlin/pl/poznan/put/mailservice/MailServiceApplication.kt
+++ mail-service/src/main/kotlin/pl/poznan/put/mailservice/MailServiceApplication.kt
@@ -2,8 +2,10 @@ package pl.poznan.put.mailservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class MailServiceApplication
 
 fun main(args: Array<String>) {
