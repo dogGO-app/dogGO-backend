311ccddf22872fe635b6b097fdf30b510addcd06
Adrian Glapiński <adriangl1997@gmail.com>
2020-10-03 15:37:43 +0200

    Configure properties - fixes

--- mail-service/build.gradle.kts
+++ mail-service/build.gradle.kts
@@ -2,6 +2,6 @@ version = "0.0.1-SNAPSHOT"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:2.3.4.RELEASE")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 }
