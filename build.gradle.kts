804342627cd224ddab6f9d06867e5b65b915bf46
Adrian Glapiński <adriangl1997@gmail.com>
2020-10-04 16:38:18 +0200

    Remove Spring Cloud Config, add Actuator and fix path filters

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ subprojects {
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        implementation("org.springframework.cloud:spring-cloud-starter-config")
         testImplementation("org.springframework.boot:spring-boot-starter-test") {
             exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         }
