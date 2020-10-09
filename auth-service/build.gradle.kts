d9406e18da3589c77ca1e1c45481bcb04bc2cb34
Adrian Glapiński <adriangl1997@gmail.com>
2020-10-09 10:27:44 +0200

    Add embedded Keycloak

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -1,9 +1,21 @@
 version = "0.0.1-SNAPSHOT"
 
+repositories {
+	maven { url = uri("https://jitpack.io") }
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.github.thomasdarimont.embedded-spring-boot-keycloak-server:embedded-keycloak-server-spring-boot-parent:2.4.0")
+	}
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	runtimeOnly("org.postgresql:postgresql")
+
+	implementation("com.github.thomasdarimont.embedded-spring-boot-keycloak-server:embedded-keycloak-server-spring-boot-starter:2.4.0")
 }
