6abc9d72150bd63590d2412357722c4108748dfd
mtarka <marta.tarka@student.put.poznan.pl>
2020-10-16 22:01:21 +0200

    Implement OpenApi on Gateway

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -4,4 +4,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springdoc:springdoc-openapi-webmvc-core:1.4.8")
+	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.4.8")
 }
