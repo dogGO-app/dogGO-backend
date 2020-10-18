643722071c9947c6f5c65e9c5f540d14fbfc136e
mtarka <marta.tarka@student.put.poznan.pl>
2020-10-18 15:59:38 +0200

    pr fix

--- dog-lover-service/src/main/kotlin/pl/poznan/put/dogloverservice/exceptions/ServiceException.kt
+++ dog-lover-service/src/main/kotlin/pl/poznan/put/dogloverservice/exceptions/ServiceException.kt
@@ -3,9 +3,9 @@ package pl.poznan.put.dogloverservice.exceptions
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 
-open class ServiceExceptions(
+open class ServiceException(
         httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
         override val message: String
 ) : ResponseStatusException(httpStatus, message)
 
-class DogLoverNotFoundException : ServiceExceptions(HttpStatus.NOT_FOUND, "Dog lover profile not found! It may be not created yet.")
+class DogLoverNotFoundException : ServiceException(HttpStatus.NOT_FOUND, "Dog lover profile not found! It may be not created yet.")
