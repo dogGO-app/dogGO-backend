01f07c6c14302ce915196dbdcea7a6434a556908
Adrian Glapiński <adriangl1997@gmail.com>
2020-11-26 19:45:00 +0100

    Create springDoc version variable and bump springDoc to 1.5.0

--- dog-lover-service/build.gradle.kts
+++ dog-lover-service/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
 	implementation("org.springframework.security:spring-security-oauth2-resource-server")
 	implementation("org.springframework.security:spring-security-oauth2-jose")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	implementation("org.springdoc:springdoc-openapi-webmvc-core:1.4.8")
+	implementation("org.springdoc:springdoc-openapi-webmvc-core:${Versions.springDoc}")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
