ebdae47c80260e66550c29db22ce7f4f192d561f
mtarka <marta.tarka@student.put.poznan.pl>
2020-12-06 21:52:00 +0100

    extend dogLoverProfileDTO by id

--- dog-lover-service/src/main/kotlin/pl/poznan/put/dogloverservice/modules/doglover/dto/DogLoverProfileDTO.kt
+++ dog-lover-service/src/main/kotlin/pl/poznan/put/dogloverservice/modules/doglover/dto/DogLoverProfileDTO.kt
@@ -1,8 +1,10 @@
 package pl.poznan.put.dogloverservice.modules.doglover.dto
 
+import java.util.UUID
 import pl.poznan.put.dogloverservice.modules.doglover.DogLover
 
 data class DogLoverProfileDTO(
+        val id: UUID,
         val nickname: String,
         val firstName: String,
         val lastName: String,
@@ -11,6 +13,7 @@ data class DogLoverProfileDTO(
         val likesCount: Int
 ) {
     constructor(dogLover: DogLover) : this(
+            id = dogLover.id,
             nickname = dogLover.nickname,
             firstName = dogLover.firstName,
             lastName = dogLover.lastName,
