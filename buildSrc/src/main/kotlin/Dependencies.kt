5a0c04eecb2d5138b5389c146ac4b475f5fbd36d
Adrian Glapiński <adriangl1997@gmail.com>
2020-12-20 16:55:38 +0100

    Clean up dependencies and test class

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,6 +4,7 @@ object Versions {
     const val mockK = "1.10.2"
     const val springDoc = "1.5.0"
     const val caffeine = "2.8.8"
+    const val h2 = "1.4.200"
 }
 
 object Libs {
@@ -11,6 +12,7 @@ object Libs {
             "io.kotest:kotest-runner-junit5:${Versions.kotest}",
             "io.kotest:kotest-assertions-core:${Versions.kotest}",
             "io.kotest:kotest-property:${Versions.kotest}",
+            "io.kotest:kotest-extensions-spring:${Versions.kotest}",
             "io.mockk:mockk:${Versions.mockK}"
     )
 }
--- dog-lover-service/src/test/kotlin/pl/poznan/put/dogloverservice/modules/mapmarker/MapMarkerSpringBootTest.kt
+++ dog-lover-service/src/test/kotlin/pl/poznan/put/dogloverservice/modules/mapmarker/MapMarkerSpringBootTest.kt
@@ -1,39 +1,26 @@
 package pl.poznan.put.dogloverservice.modules.mapmarker
 
-import io.kotest.core.listeners.TestListener
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.spring.SpringListener
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 import javax.transaction.Transactional
 
-@RunWith(SpringRunner::class)
 @Transactional
 @SpringBootTest
-class MapMarkerSpringBootTest : FunSpec() {
+class MapMarkerSpringBootTest(
+        var mapMarkerService: MapMarkerService,
+        var mapMarkerRepository: MapMarkerRepository
+) : FunSpec({
+    test("Should save map marker") {
+        mapMarkerRepository.save(MapMarkerData.parkKonin)
 
-    override fun listeners(): List<TestListener> {
-        return listOf(SpringListener)
+        mapMarkerService.getAllMapMarkers().size shouldBe 1
     }
 
-    @Autowired
-    private lateinit var mapMarkerService: MapMarkerService
-
-    @Autowired
-    private lateinit var mapMarkerRepository: MapMarkerRepository
-
-    init {
-        test("Should save map marker") {
-            mapMarkerRepository.save(MapMarkerData.parkKonin)
-
-            mapMarkerService.getAllMapMarkers().size shouldBe 1
-        }
-
-        test("Should be empty") {
-            mapMarkerService.getAllMapMarkers()
-        }
+    test("Should be empty") {
+        mapMarkerService.getAllMapMarkers()
     }
+}) {
+    override fun listeners() = listOf(SpringListener)
 }
